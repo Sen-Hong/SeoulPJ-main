3e94f7ac3b2f933929e685c0df2fe5a25e9a74dc
Sen-Hong <senhongpower@gmail.com>
2023-05-12 15:34:15 +0900

    https://github.com/Birdtr222/SeoulPJ.git

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Push test를 위한 메세지 입니다.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textstyle:{
+    // test를 위한 주석
+  }
+});
